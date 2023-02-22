9d0f4b1ff0eda6731c44b3227bf0a92cf4f29aed
Shawn <shawnkrise@gmail.com>
2023-02-21 18:05:48 -0700

    first commit

---
+++ src/components/Instruction.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Instruction.css";
+
+export const Instruction = () => {
+  return (
+    <div className="instruction">
+    Note: The Tudo List will clear if you refresh or leave the page.
+  </div>
+  )
+}
---
+++ src/components/Name.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Name.css";
+
+export const Name = () => {
+  return (
+    <div className="name">
+    SHAWN KRISE
+  </div>
+  )
+}
